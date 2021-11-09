3f5a3e3a107a54e24d40bdc75bd7acb7c81f6cc6
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2021-11-09 08:13:13 -0300

    Initial commit

---
+++ src/main/java/com/example/ContextsInSpring/ContextsInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContextsInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextsInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextsInSpringApplication.class, args);
+	}
+
+}
